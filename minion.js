6395e296dcc00239625506a095dd60cea5491053
Gaurav <gauravdey625@gmail.com>
2020-11-29 21:01:37 +0530

    First Commit

---
+++ minion.js
@@ -0,0 +1,24 @@
+var minion = 'https://api.funtranslations.com/translate/';
+var submit = document.getElementById('submit');
+var display = document.getElementById('display');
+var language = document.getElementById('language');
+submit.addEventListener('click',()=>{
+    var text = document.getElementById('inputtext').value;
+    var addon = minion+language.value+'.json';
+    var get = addon+'?text='+text;
+    console.log(get);
+    getrequestvalue(get);
+});
+
+function getrequestvalue(url) {
+    display.innerText = 'Please wait.....';
+    fetch(url)
+    .then(response => response.json())
+    .then(json =>{
+        display.innerText = json.contents.translated;
+    })
+    .catch(data =>{
+        display.innerText = '';
+        alert("Too Many Requests: Rate limit of 5 requests per hour exceeded");
+    })
+}
